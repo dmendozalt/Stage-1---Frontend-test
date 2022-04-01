6384e9e9acc4be79653dc344851db0b8f81a13a9
dmendozalt <dmendoza@lean-tech.io>
2022-04-01 12:29:17 -0500

    fix: update point 1 function

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 let docs = [];
 
 async function getData() {
-  return await fetch("https://api.plos.org/search?q=title:DNA")
+  return await fetch('https://api.plos.org/search?q=title:DNA')
     .then((response) => response.json())
     .then((json) => {
       docs = json.response.docs;
     });
 }
 
-function filterDocsBy(property, target) {
+function filterByArticleType(target) {
   // 1. Develop a function that prints by console the data filtering by "article_type" and get all the data that are different from "Research Article".
   // ○ Extra points, receive the value to filter by parameters.
-  return docs.filter((doc) => doc[property] === target);
+  return docs.filter((doc) => doc.article_type === target);
 }
 
 async function get() {
   await getData();
   console.log(docs);
-  console.log("Punto 1", filterDocsBy("article_type", "Research Article"));
+  console.log('Punto 1', filterByArticleType('Research Article'));
 }
 get();
